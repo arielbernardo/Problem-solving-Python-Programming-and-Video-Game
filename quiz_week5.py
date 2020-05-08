563517144de11f2e88bb5d2afae0f85508202ac0
arielBernardo <arielbernardo@icloud.com>
2020-05-08 12:26:18 +0400

    Update quiz_week5.py

--- quiz_week5.py
+++ quiz_week5.py
@@ -26,7 +26,32 @@ except ValueError:
         print(val, 'is not a non-negative integer')
     except ValueError:
         pass
+       
         
         
         
-        
+# Programming (keyword operator, short circuit evaluation, unary expression, and operator precedence)
+# Write a Python program that asks the user to input an integer. If the user enters an integer, output the negative of that integer. If the user does not enter an integer, output a warning message. For this question a valid integer consists of one or more digits (0-9) with an optional leading minus sign (-).
+
+# For example, here are three sample program runs:
+# Enter an integer >-43
+# The negative of -43 is 43
+
+# Enter an integer >43
+# The negative of 43 is -43Enter an integer >fortythree
+
+# fortythree is not an integer
+
+n = input('Enter an integer >')
+try:
+    val = int(n)
+    if val < 0:
+        print('The negative of {} is {}'.format(val, abs(val)))
+    elif val > 0:
+        print('The negative of {} is {}'.format(val, -abs(val)))
+except ValueError:
+    try:
+        val = n
+        print('{} is not an integer'.format(val))
+    except ValueError:
+        pass
